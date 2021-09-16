91710a266ea3a1f1080c629cbcf4587ab7da9a5b
Zhymmo <89058801+Zhymmo@users.noreply.github.com>
2021-09-16 23:34:14 +0800

    Create 通讯录——链表.h

---
+++ 通讯录——链表.h
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdlib.h>
+#include<stdio.h>
+#include<errno.h>
+
+enum opr
+{
+	EXIT,
+	ADD,
+	DELETE,
+	SEARCH,
+	CHANGE,
+	SHOW,
+	SORT
+};
+
+typedef struct People//结构体表示联系人
+{
+	char name[20];
+	char tel[12];
+	char sex[5] ;
+	int age;
+}Peo;
+typedef struct Lnode //单链表，包括联系人信息和后继节点指针
+{
+	Peo info;
+	struct L* next;
+}L,*P;
+
+void add(P c);
+void show(P c);
+void Sort(P c);
+void Delete(P c, char* peo);
+void Search(P c, char* peo);
+void Change(P c, char* peo);
